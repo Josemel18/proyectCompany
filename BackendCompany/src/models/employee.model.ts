b9f5cb979807f5d803edfb83140de1dd4eba9dad
JoseMel <josemel18@gmail.com>
2021-11-16 12:44:58 -0500

    The employee model was created

---
+++ BackendCompany/src/models/employee.model.ts
@@ -0,0 +1,70 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Employee extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  birthday: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  salary: number;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  isExecutive: boolean;
+
+
+  constructor(data?: Partial<Employee>) {
+    super(data);
+  }
+}
+
+export interface EmployeeRelations {
+  // describe navigational properties here
+}
+
+export type EmployeeWithRelations = Employee & EmployeeRelations;
