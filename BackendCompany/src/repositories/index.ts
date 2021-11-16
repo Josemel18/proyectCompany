7eeb6e2cf1a216f0a7f9e8dde97a778fbb2b2dc7
JoseMel <josemel18@gmail.com>
2021-11-16 13:00:23 -0500

    the repositories (of client, company, employee, product and sale) were created

---
+++ BackendCompany/src/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from './client.repository';
+export * from './company.repository';
+export * from './employee.repository';
+export * from './product.repository';
+export * from './sale.repository';
